44f7894d5845b922966f68f5fcbddb4b0ccc1a25
ALmightxxx <68779543+almightxxx@users.noreply.github.com>
2020-09-15 13:30:25 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -12,4 +12,8 @@ with(datasub, {
   lines(Sub_metering_3~Datetime,col='Blue')
 })
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
-       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))## Saving to file
+dev.copy(png, file="plot3.png", height=480, width=480)
+
+dev.off()
+
